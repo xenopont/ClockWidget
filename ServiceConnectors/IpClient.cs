590049447314c3f39766f414e391e8c3d3d55d8b
Sergei Kovalenko <smith404@live.com>
2025-02-16 22:39:31 +0100

    ip client implementation

---
+++ ServiceConnectors/IpClient.cs
@@ -0,0 +1,31 @@
+﻿namespace ServiceConnectors;
+
+public static class IpClient
+{
+    public static async Task<string?> GetExternalIp()
+    {
+        var millisecondsSinceLastRequest = DateTime.Now.Subtract(_lastUpdate).TotalMilliseconds;
+        if (millisecondsSinceLastRequest < UpdateIntervalMilliseconds)
+        {
+            return _currentValue;
+        }
+        
+        try
+        {
+            _currentValue = await Client.GetStringAsync("https://api.ipify.org");
+            _lastUpdate = DateTime.Now;
+        }
+        catch (HttpRequestException)
+        {
+            return null;
+        }
+        
+        return _currentValue;
+    }
+    
+    private const int UpdateIntervalMilliseconds = 5 * 60 * 1000;
+    
+    private static readonly HttpClient Client = new();
+    private static string? _currentValue;
+    private static DateTime _lastUpdate = DateTime.MinValue;
+}
