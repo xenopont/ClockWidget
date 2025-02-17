1064e6506f5c074fa18907b3975a6e88d32f1865
Sergei Kovalenko <smith404@live.com>
2025-02-17 23:29:11 +0100

    ip client rewritten for better testability

--- ServiceConnectors/IpClient.cs
+++ ServiceConnectors/IpClient.cs
@@ -1,31 +1,56 @@
 ﻿namespace ServiceConnectors;
 
-public static class IpClient
+public interface IHttpStringClient
 {
-    public static async Task<string?> GetExternalIp()
+    Task<string> GetStringAsync(string uri);
+}
+
+public interface INowProvider
+{
+    DateTime Now { get; }
+}
+
+public class IpClient(IHttpStringClient? httpStringClient = null, INowProvider? nowProvider = null)
+{
+    private const int InvalidateIntervalSeconds = 5 * 60;
+
+    private static string? _currentValue;
+    private static DateTime _lastUpdate = DateTime.MinValue;
+
+    private readonly IHttpStringClient _httpClient = httpStringClient ?? new DefaultHttpClient();
+    private readonly INowProvider _nowProvider = nowProvider ?? new DefaultNowProvider();
+
+    public async Task<string?> GetExternalIp()
     {
-        var millisecondsSinceLastRequest = DateTime.Now.Subtract(_lastUpdate).TotalMilliseconds;
-        if (millisecondsSinceLastRequest < UpdateIntervalMilliseconds)
-        {
-            return _currentValue;
-        }
-        
+        var updatingMoment = _nowProvider.Now;
+        var secondsSinceLastRequest = updatingMoment.Subtract(_lastUpdate).TotalSeconds;
+        if (secondsSinceLastRequest < InvalidateIntervalSeconds) return _currentValue;
+
         try
         {
-            _currentValue = await Client.GetStringAsync("https://api.ipify.org");
-            _lastUpdate = DateTime.Now;
+            _currentValue = await _httpClient.GetStringAsync("https://api.ipify.org");
+            _lastUpdate = updatingMoment;
         }
         catch (HttpRequestException)
         {
             return null;
         }
-        
+
         return _currentValue;
     }
-    
-    private const int UpdateIntervalMilliseconds = 5 * 60 * 1000;
-    
-    private static readonly HttpClient Client = new();
-    private static string? _currentValue;
-    private static DateTime _lastUpdate = DateTime.MinValue;
+
+    private class DefaultHttpClient : IHttpStringClient
+    {
+        private readonly HttpClient _httpClient = new();
+
+        public Task<string> GetStringAsync(string uri)
+        {
+            return _httpClient.GetStringAsync(uri);
+        }
+    }
+
+    private class DefaultNowProvider : INowProvider
+    {
+        public DateTime Now => DateTime.Now;
+    }
 }
